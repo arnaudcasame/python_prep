a2f3328a0ce5218fd0499039600d4abd509d75d9
Tizekod <noon.tizekod@gmail.com>
2024-02-12 23:03:52 -0500

    Fixes wrong type annotation

--- leetcode/majority_element_169/majority_element.py
+++ leetcode/majority_element_169/majority_element.py
@@ -1,11 +1,5 @@
-def majority_element(nums: list):
-    bucket = dict()
-    majority = 0
-    for num in nums:
-        if num in bucket:
-            bucket[num] = bucket[num] + 1
-        else:
-            bucket[num] = 1
-        if bucket.get(num) > len(nums)//2:
-            majority = num
-    return majority
+from typing import List
+
+
+def majority_element(nums: List[int])->int:
+    return 0
