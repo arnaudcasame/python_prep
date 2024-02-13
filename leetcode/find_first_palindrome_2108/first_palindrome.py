11a7c2249a8077f9806b194867af34e6483387df
Tizekod <noon.tizekod@gmail.com>
2024-02-13 00:53:25 -0500

    Solves leetcode problem 2108

--- leetcode/find_first_palindrome_2108/first_palindrome.py
+++ leetcode/find_first_palindrome_2108/first_palindrome.py
@@ -2,4 +2,18 @@ from typing import List
 
 
 def firstPalindrome(words: List[str]) -> str:
-    return
+    for word in words:
+        if isPalindrome(word):
+            return word
+    return ''
+
+
+def isPalindrome(word: str) -> bool:
+    length = len(word)
+    n = (length//2)+1 if length%2 == 0 else round(length/2)+1
+    for index in range(0, n):
+        left = index
+        right = length - 1 - index
+        if word[left] != word[right]:
+            return False
+    return True
--- leetcode/find_first_palindrome_2108/test_first_palindrome.py
+++ leetcode/find_first_palindrome_2108/test_first_palindrome.py
@@ -1,14 +1,14 @@
 import pytest
 from leetcode.find_first_palindrome_2108.first_palindrome import firstPalindrome
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer1():
     assert firstPalindrome(["abc","car","ada","racecar","cool"]) == "ada"
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer2():
     assert firstPalindrome(["notapalindrome","racecar"]) == "racecar"
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer3():
     assert firstPalindrome(["def","ghi"]) == ""
