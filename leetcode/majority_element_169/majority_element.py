e9bf306ea8d5288ed2ce89ce4c31bc0571435c8a
Tizekod <noon.tizekod@gmail.com>
2024-02-13 01:44:51 -0500

    Solves leetcode problem 169

--- leetcode/majority_element_169/majority_element.py
+++ leetcode/majority_element_169/majority_element.py
@@ -1,5 +1,10 @@
 from typing import List
 
-
 def majority_element(nums: List[int])->int:
-    return 0
+    item = 0
+    majority = 0
+    for num in nums:
+        if majority == 0:
+            item = num
+        majority += 1 if num == item else -1
+    return item
--- leetcode/majority_element_169/test_majority_element.py
+++ leetcode/majority_element_169/test_majority_element.py
@@ -1,10 +1,9 @@
-import pytest
 from leetcode.majority_element_169.majority_element import majority_element
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer1():
     assert majority_element([3,2,3]) == 3
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer2():
     assert majority_element([2,2,1,1,1,2,2]) == 2
