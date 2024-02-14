fb459d0a416dabdc61e8a9d0892056571511cc7b
Tizekod <noon.tizekod@gmail.com>
2024-02-14 00:56:57 -0500

    Optimizes Leetcode problem 2149

--- leetcode/rearrange_array_elements_by_sign_2149/rearrange_array.py
+++ leetcode/rearrange_array_elements_by_sign_2149/rearrange_array.py
@@ -1,14 +1,14 @@
 from typing import List
 
 def rearrangeArray(nums: List[int]) -> List[int]:
-    bucket = []
+    bucket = [0] * len(nums)
     p, n = 0, 1
     for i in range(0, len(nums)):
         num = nums[i]
         if num < 0:
-            bucket.insert(n, num)
+            bucket[n] = num
             n += 2
         else:
-            bucket.insert(p, num)
+            bucket[p] = num
             p += 2
     return bucket
