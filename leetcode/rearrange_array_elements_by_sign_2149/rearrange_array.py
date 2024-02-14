79c3781f866359e116e5c73d5d775faeb2ba5dd3
Tizekod <noon.tizekod@gmail.com>
2024-02-14 00:49:58 -0500

    Solves leetcode problem 2149

--- leetcode/rearrange_array_elements_by_sign_2149/rearrange_array.py
+++ leetcode/rearrange_array_elements_by_sign_2149/rearrange_array.py
@@ -1,4 +1,14 @@
 from typing import List
 
 def rearrangeArray(nums: List[int]) -> List[int]:
-    return []
+    bucket = []
+    p, n = 0, 1
+    for i in range(0, len(nums)):
+        num = nums[i]
+        if num < 0:
+            bucket.insert(n, num)
+            n += 2
+        else:
+            bucket.insert(p, num)
+            p += 2
+    return bucket
--- leetcode/rearrange_array_elements_by_sign_2149/test_rearrange_array.py
+++ leetcode/rearrange_array_elements_by_sign_2149/test_rearrange_array.py
@@ -1,10 +1,10 @@
 import pytest
 from rearrange_array import rearrangeArray
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer1():
     assert rearrangeArray([3,1,-2,-5,2,-4]) == [3,-2,1,-5,2,-4]
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+
 def test_answer2():
     assert rearrangeArray([-1,1]) == [1,-1]
