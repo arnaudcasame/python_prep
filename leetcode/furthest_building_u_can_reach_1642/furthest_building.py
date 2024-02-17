eef9b6b52ba4200f740153bf6641b0b14f38e003
Tizekod <noon.tizekod@gmail.com>
2024-02-17 00:26:45 -0500

    Solves leetcode problem #1642

--- leetcode/furthest_building_u_can_reach_1642/furthest_building.py
+++ leetcode/furthest_building_u_can_reach_1642/furthest_building.py
@@ -1,4 +1,17 @@
+import heapq
 from typing import List
 
 def furthestBuilding(heights: List[int], bricks: int, ladders: int) -> int:
-    return 0;
+    heap = []
+    for i in range(0, len(heights)-1):
+        if heights[i+1] - heights[i] <= 0:
+            continue
+        bricks -= heights[i+1] - heights[i]
+        heapq.heappush(heap, -(heights[i+1] - heights[i]))
+
+        if bricks < 0:
+            if ladders == 0:
+                return i
+            ladders -= 1
+            bricks += -heapq.heappop(heap)
+    return len(heights) - 1
--- leetcode/furthest_building_u_can_reach_1642/test_furthest_building.py
+++ leetcode/furthest_building_u_can_reach_1642/test_furthest_building.py
@@ -2,14 +2,26 @@ import pytest
 from leetcode.furthest_building_u_can_reach_1642.furthest_building import furthestBuilding
 
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_1():
     assert furthestBuilding([4,2,7,6,9,14,12], 5, 1) == 4
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_2():
     assert furthestBuilding([4,12,2,7,3,18,20,3,19], 10, 2) == 7
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_3():
     assert furthestBuilding([14,3,19,3], 17, 0) == 3
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer_4():
+    assert furthestBuilding([3,19], 87, 1) == 1
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer_5():
+    assert furthestBuilding([1, 2], 0, 0) == 0
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer_6():
+    assert furthestBuilding([1,5,1,2,3,4,10000], 4, 1) == 5
