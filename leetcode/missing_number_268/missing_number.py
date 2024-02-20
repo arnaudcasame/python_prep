db775ed6e6494204d9259141c6bfec7d262baaee
Tizekod <noon.tizekod@gmail.com>
2024-02-19 22:03:28 -0500

    Solves the leetcode problem #268

--- leetcode/missing_number_268/missing_number.py
+++ leetcode/missing_number_268/missing_number.py
@@ -1,4 +1,18 @@
 from typing import List
 
-def missingNumber(self, nums: List[int]) -> int:
-    return 0
+def missingNumber(nums: List[int]) -> int:
+    bucket = {}
+    for num in nums:
+        bucket[num] = 1
+    for i in range(0, len(nums)+1):
+        if i not in nums:
+            return i
+    return nums[-1]+1
+
+def missingNumberO(nums: List[int]) -> int:
+        missing = 0
+        for num in nums:
+            missing ^= num
+        for i in range(len(nums) + 1):
+            missing ^= i
+        return missing
--- leetcode/missing_number_268/test_missing_number.py
+++ leetcode/missing_number_268/test_missing_number.py
@@ -1,14 +1,14 @@
 import pytest
 from leetcode.missing_number_268.missing_number import missingNumber
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert missingNumber([3,0,1]) == 2
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert missingNumber([0,1]) == 2
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer3():
     assert missingNumber([9,6,4,2,3,5,7,0,1]) == 8
