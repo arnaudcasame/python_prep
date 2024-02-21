67b020e0edea0a3b2a04caf0ce316978b583e11c
Tizekod <noon.tizekod@gmail.com>
2024-02-20 21:36:27 -0500

    Solves the leetcode problem #201

--- leetcode/bitwise_AND_numbers_range_201/bitwise_and_numbers.py
+++ leetcode/bitwise_AND_numbers_range_201/bitwise_and_numbers.py
@@ -1,3 +1,10 @@
 
 def rangeBitwiseAnd(left: int, right: int) -> int:
-    return 0
+    rightShiftCount = 0
+
+    while left < right:
+        left >>= 1
+        right >>= 1
+        rightShiftCount += 1
+
+    return left << rightShiftCount
