7122b01d2c7823e565e33f8fe8530d55eab846fe
Tizekod <noon.tizekod@gmail.com>
2024-02-20 21:26:34 -0500

    Adds leetcode problem #201

---
+++ leetcode/bitwise_AND_numbers_range_201/test_bitwise_and_numbers.py
@@ -0,0 +1,15 @@
+import pytest
+from bitwise_and_numbers import rangeBitwiseAnd
+
+
+@pytest.mark.skip('Unskip only for testing solution')
+def test_answer1():
+    assert rangeBitwiseAnd(5, 7) == 24
+
+@pytest.mark.skip('Unskip only for testing solution')
+def test_answer2():
+    assert rangeBitwiseAnd(0, 0) == 0
+
+@pytest.mark.skip('Unskip only for testing solution')
+def test_answer3():
+    assert rangeBitwiseAnd(1, 2147483647) == 0
