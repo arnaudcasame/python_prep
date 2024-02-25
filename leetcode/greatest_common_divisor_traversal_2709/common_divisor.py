3e8d0f901e4abdf9c534e03d5e4aa9aab2e82b90
Tizekod <noon.tizekod@gmail.com>
2024-02-25 03:41:22 -0500

    Solves leetcode problem #2709

--- leetcode/greatest_common_divisor_traversal_2709/common_divisor.py
+++ leetcode/greatest_common_divisor_traversal_2709/common_divisor.py
@@ -1,4 +1,46 @@
 from typing import List
 
 def canTraverseAllPairs(nums: List[int]) -> bool:
-    return False
+    uf = UnionFind(len(nums))
+    factor_index = {}
+    for i, n in enumerate(nums):
+        f = 2
+        while f * f <= n:
+            if n % f == 0:
+                if f in factor_index:
+                    uf.union(i, factor_index[f])
+                else:
+                    factor_index[f] = i
+                while n%f == 0:
+                    n = n // f
+            f += 1
+        if n > 1:
+            if n in factor_index:
+                uf.union(i, factor_index[n])
+            else:
+                factor_index[n] = i
+    return uf.count == 1
+
+
+class UnionFind():
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+        self.count  =  n
+        
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+            self.rank[py] += self.rank[px]
+        else:
+            self.parent[py] = px
+            self.rank[px] += self.rank[py]
+        self.count -= 1
+        
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
--- leetcode/greatest_common_divisor_traversal_2709/test_common_divisor.py
+++ leetcode/greatest_common_divisor_traversal_2709/test_common_divisor.py
@@ -2,30 +2,30 @@ import pytest
 from leetcode.greatest_common_divisor_traversal_2709.common_divisor import canTraverseAllPairs
 
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_1():
     assert canTraverseAllPairs([2,3,6]) == True
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_2():
     assert canTraverseAllPairs([3,9,5]) == False
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_3():
     assert canTraverseAllPairs([4,3,12,8]) == True
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_4():
     assert canTraverseAllPairs([28,39]) == False
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_5():
     assert canTraverseAllPairs([21,88,75]) == False
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_6():
     assert canTraverseAllPairs([10]) == True
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer_7():
     assert canTraverseAllPairs([1,1]) == False
