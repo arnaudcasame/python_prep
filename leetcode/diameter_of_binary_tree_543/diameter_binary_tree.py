4cad4a16dfbf2d08b1469102b5152a6f3fe30151
Tizekod <noon.tizekod@gmail.com>
2024-03-01 00:18:43 -0500

    Solves leetcode problem #543

--- leetcode/diameter_of_binary_tree_543/diameter_binary_tree.py
+++ leetcode/diameter_of_binary_tree_543/diameter_binary_tree.py
@@ -2,4 +2,18 @@ from typing import Optional
 
 
 def diameterOfBinaryTree(root: Optional[int]) -> int:
-    return 0
+
+    
+
+    return dfs(root, 0)
+
+def dfs(root, result) -> int:
+    if not root:
+        return -1
+    
+    left = dfs(root.left, result)
+    right = dfs(root.right, result)
+
+    result[0] = max(result[0], left + right + 2)
+
+    return -1 + max(left, right)
--- leetcode/diameter_of_binary_tree_543/test_diameter_binary_tree.py
+++ leetcode/diameter_of_binary_tree_543/test_diameter_binary_tree.py
@@ -2,10 +2,10 @@ import pytest
 
 from diameter_binary_tree import diameterOfBinaryTree
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert diameterOfBinaryTree([1,2,3,4,5]) == 3
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert diameterOfBinaryTree([1,2]) == 1
