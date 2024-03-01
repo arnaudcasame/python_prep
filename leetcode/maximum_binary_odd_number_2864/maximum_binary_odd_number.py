4f8cbefdd8d1cf10577cf22a5e6600e2835c98bf
Tizekod <noon.tizekod@gmail.com>
2024-03-01 00:36:28 -0500

    Solves leetcode problem #2864

--- leetcode/maximum_binary_odd_number_2864/maximum_binary_odd_number.py
+++ leetcode/maximum_binary_odd_number_2864/maximum_binary_odd_number.py
@@ -1,3 +1,15 @@
 
 def maximumOddBinaryNumber(s: str) -> str:
-    return ''
+    arr = [ch for ch in s]
+
+    left = 0
+    right = 0
+
+    while right < len(arr):
+        if arr[right] == '1':
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+        right += 1
+    arr[left-1], arr[right-1] = arr[right-1], arr[left-1]
+
+    return ''.join(arr)
--- leetcode/maximum_binary_odd_number_2864/test_maximum_binary_odd_number.py
+++ leetcode/maximum_binary_odd_number_2864/test_maximum_binary_odd_number.py
@@ -1,14 +1,14 @@
 import pytest
 from maximum_binary_odd_number import maximumOddBinaryNumber
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert maximumOddBinaryNumber('010') == '001'
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert maximumOddBinaryNumber('0101') == '1001'
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer3():
     assert maximumOddBinaryNumber('1') == '1'
