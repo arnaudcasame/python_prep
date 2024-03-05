f360b137fa1ec36742bb3c5ed9e2d89cdc020c13
Tizekod <noon.tizekod@gmail.com>
2024-03-05 00:14:40 -0500

    Solves leetcode problem #1750

--- leetcode/minimum_length_of_string_after_deleting_similar_ends_1750/minimum_length_string.py
+++ leetcode/minimum_length_of_string_after_deleting_similar_ends_1750/minimum_length_string.py
@@ -1,3 +1,22 @@
 
 def minimumLength(s: str) -> int:
-    return -1
+    left, right = 0, len(s)-1
+
+    while left < right:
+        if s[left] == s[right] and left != right:
+            letter = s[left]
+
+            while s[left] == letter and left < right:
+                    left += 1
+            while s[right] == letter and left < right:
+                    right -= 1
+
+            print(s, left, right)
+            s = s[left+1:] if left == right else s[left:]
+            s = s[:right+1-left]
+            print(s, left, right)
+            left = 0
+            right = len(s)-1
+        else:
+            break
+    return len(s)
