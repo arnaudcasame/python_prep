6b846899769b9181556a72d46eca7fae0ee9f473
Tizekod <noon.tizekod@gmail.com>
2024-03-07 20:28:13 -0500

    Solves leetcode problem #3005

--- leetcode/count_elements_maximum_frequency_3005/count_elements_w_maximum_frequency.py
+++ leetcode/count_elements_maximum_frequency_3005/count_elements_w_maximum_frequency.py
@@ -1,4 +1,17 @@
 from typing import List
 
 def maxFrequencyElements(nums: List[int]) -> int:
-    return 0
+    bucket = {}
+    for num in nums:
+        bucket[num] = 1 if num not in bucket else bucket[num] + 1
+    # print(bucket)
+    lastFreq = 0
+    count = 0
+    for key in bucket:
+        if lastFreq < bucket[key]:
+            lastFreq = bucket[key]
+            count = 0
+            count += bucket[key]
+        elif lastFreq == bucket[key]:
+            count += bucket[key]
+    return count
