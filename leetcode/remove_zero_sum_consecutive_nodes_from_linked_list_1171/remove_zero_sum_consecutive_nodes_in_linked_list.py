dea230a869a515c3e98f3d8c8473652b8dee868c
Tizekod <noon.tizekod@gmail.com>
2024-03-12 21:31:59 -0400

    Solves Leetcode problem #1171

--- leetcode/remove_zero_sum_consecutive_nodes_from_linked_list_1171/remove_zero_sum_consecutive_nodes_in_linked_list.py
+++ leetcode/remove_zero_sum_consecutive_nodes_from_linked_list_1171/remove_zero_sum_consecutive_nodes_in_linked_list.py
@@ -3,4 +3,25 @@ from data_structures.list_node import ListNode
 
 
 def removeZeroSumSublists(head: Optional[ListNode]) -> Optional[ListNode]:
-    return []
+    cumul: int = 0
+    bucket: dict[int, ListNode] = {}
+    tmp: ListNode = ListNode(0, head)
+    bucket[0] = tmp
+
+    while head != None:
+        cumul = cumul + head.val
+
+        if cumul in bucket:
+            tmpCumul = cumul
+            tmpRemove = bucket[cumul].next
+
+            while tmpRemove != head:
+                tmpCumul += tmpRemove.val
+                bucket.pop(tmpCumul)
+                tmpRemove = tmpRemove.next
+            bucket[cumul].next = head.next
+        else:
+            bucket[cumul] = head
+
+        head = head.next
+    return tmp.next
--- leetcode/remove_zero_sum_consecutive_nodes_from_linked_list_1171/test_remove_zero_sum_consecutive_nodes_in_linked_list.py
+++ leetcode/remove_zero_sum_consecutive_nodes_from_linked_list_1171/test_remove_zero_sum_consecutive_nodes_in_linked_list.py
@@ -3,19 +3,19 @@ from remove_zero_sum_consecutive_nodes_in_linked_list import removeZeroSumSublis
 
 import pytest
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     head = ListNode.fromList([1,2,-3,3,1])
     result = removeZeroSumSublists(head)
     assert ListNode.toList(result) == [3,1]
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     head = ListNode.fromList([1,2,3,-3,4])
     result = removeZeroSumSublists(head)
     assert ListNode.toList(result) == [1,2,4]
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer3():
     head = ListNode.fromList([1,2,3,-3,-2])
     result = removeZeroSumSublists(head)
