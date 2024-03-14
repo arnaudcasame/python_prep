596dcaa487793b65a6d00d2130fba6dbb9a17d04
Tizekod <noon.tizekod@gmail.com>
2024-03-14 02:33:19 -0400

    Solves Leetcode problem #392

--- leetcode/is_subsequence_392/test_is_subsequence.py
+++ leetcode/is_subsequence_392/test_is_subsequence.py
@@ -2,10 +2,10 @@ from is_subsequence import isSubsequence
 
 import pytest
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert isSubsequence('abc', 'ahbgdc') == True
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert isSubsequence('axc', 'ahbgdc') == False
