84c5de85fba73bd7766eac79b476412995845f05
Tizekod <noon.tizekod@gmail.com>
2024-03-15 00:32:11 -0400

    Solves Leetcode problem #11

--- leetcode/container_with_most_water_11/most_water.py
+++ leetcode/container_with_most_water_11/most_water.py
@@ -2,4 +2,13 @@ from typing import List
 
 
 def maxArea(height: List[int]) -> int:
-    return 0
+    i, j = 0, len(height)-1
+    area = 0
+    while i < j:
+        lesser = height[i] if height[i] < height[j] else height[j]
+        area = max(area, lesser * (j-i))
+        if height[i] < height[j]:
+            i += 1
+        else:
+            j -= 1
+    return area
