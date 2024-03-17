f29356b012fdf2e2ed623952fab4b574ddecf2ce
Tizekod <noon.tizekod@gmail.com>
2024-03-16 20:27:43 -0400

    Solves the Leetcode problem #525

--- leetcode/contiguous_array_525/contiguous_array.py
+++ leetcode/contiguous_array_525/contiguous_array.py
@@ -2,4 +2,21 @@ from typing import List
 
 
 def findMaxLength(nums: List[int]) -> int:
-    return -1
+    o, z = 0, 0
+    bucket = {}
+    count = 0
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            z += 1
+        else:
+            o += 1
+        
+        if (o-z) not in bucket:
+            bucket[o-z] = i
+        
+        if o == z:
+            count = o + z
+        else:
+            index = bucket[o-z]
+            count = max(count, i - index)
+    return count
