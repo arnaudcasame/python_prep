411c163ecb00ed25d17576cccf7049e65a5bba93
Tizekod <noon.tizekod@gmail.com>
2024-03-17 15:53:59 -0400

    Solves the Leetcode problem #57

--- leetcode/insert_interval_57/insert_interval.py
+++ leetcode/insert_interval_57/insert_interval.py
@@ -2,4 +2,22 @@ from typing import List
 
 
 def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-    return []
+    result = []
+    i = 0
+
+    while i < len(intervals):
+        if newInterval[1] < intervals[i][0]:
+            break
+        elif newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        else:
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        i += 1
+    
+    result.append(newInterval)
+
+    while i < len(intervals):
+        result.append(intervals[i])
+        i += 1
+
+    return result
--- leetcode/insert_interval_57/test_insert_interval.py
+++ leetcode/insert_interval_57/test_insert_interval.py
@@ -1,10 +1,26 @@
 from insert_interval import insert
 import pytest
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert insert([[1,3],[6,9]], [2, 5]) == [[1, 5], [6, 9]]
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]) == [[1,2],[3,10],[12,16]]
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer3():
+    assert insert([[1,5]], [2,7]) == [[1,7]]
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer4():
+    assert insert([], [5,7]) == [[5,7]]
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer5():
+    assert insert([[1,5]], [1,7]) == [[1,7]]
+
+# @pytest.mark.skip(reason='Unskip only to test solution')
+def test_answer6():
+    assert insert([[1,5]], [0,3]) == [[0,5]]
