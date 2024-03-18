e85af993deb76ab2e58d8194939e7f6de42bcc91
Tizekod <noon.tizekod@gmail.com>
2024-03-18 00:06:03 -0400

    Solves the Leetcode problem #452

--- leetcode/minimum_number_arrows_to_burst_balloons_452/minimum_number_arrows.py
+++ leetcode/minimum_number_arrows_to_burst_balloons_452/minimum_number_arrows.py
@@ -2,4 +2,17 @@ from typing import List
 
 
 def findMinArrowShots(points: List[List[int]]) -> int:
-    return -1
+    result = len(points)
+    points.sort()
+    previous = points[0]
+
+    for i in range(1, len(points)):
+        current = points[i]
+
+        if current[0] <= previous[1]:
+            result -= 1
+            previous = [current[0], min(current[1], previous[1])]
+        else:
+            previous = current
+
+    return result
--- leetcode/minimum_number_arrows_to_burst_balloons_452/test_minimum_number_arrows.py
+++ leetcode/minimum_number_arrows_to_burst_balloons_452/test_minimum_number_arrows.py
@@ -2,14 +2,14 @@ from minimum_number_arrows import findMinArrowShots
 
 import pytest
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer1():
     assert findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]) == 2
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer2():
     assert findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]) == 4
 
-@pytest.mark.skip(reason='Unskip only to test solution')
+# @pytest.mark.skip(reason='Unskip only to test solution')
 def test_answer3():
     assert findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]) == 2
