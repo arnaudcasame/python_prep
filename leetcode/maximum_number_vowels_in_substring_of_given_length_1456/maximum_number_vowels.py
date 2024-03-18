b2bfa686666eb20cc9148315fe31c491f1bf2f9f
Tizekod <noon.tizekod@gmail.com>
2024-03-18 00:53:39 -0400

    Solves the Leetcode problem #1456

--- leetcode/maximum_number_vowels_in_substring_of_given_length_1456/maximum_number_vowels.py
+++ leetcode/maximum_number_vowels_in_substring_of_given_length_1456/maximum_number_vowels.py
@@ -3,6 +3,30 @@
 def maxVowels(s: str, k: int) -> int:
     count = 0
     vowels = set(['a', 'e', 'i', 'o', 'u'])
+    num = 0
+    for i in range(k):
+        if s[i] in vowels:
+            num += 1
+            count = max(count, num)
+        # if i == k-1:
+        #     print(s[i], count)
+
+    i, j = 1, k
+    while j < len(s):
+        # if j == k:
+        #     print(s[j], count)
+        if s[j] in vowels:
+            num += 1
+        if s[i-1] in vowels:
+            num -= 1
+        count = max(count, num)
+        i += 1
+        j += 1
+    return count
+
+def maxVowelsBruteForce(s: str, k: int) -> int:
+    count = 0
+    vowels = set(['a', 'e', 'i', 'o', 'u'])
     for i in range(len(s)):
         j = i
         num = 0
